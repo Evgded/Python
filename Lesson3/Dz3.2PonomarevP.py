2c7120a3d60ffc1f9aa0203bab8a7851ba1e6002
Evgded <evgded@yandex.ru>
2020-11-26 22:46:37 +0300

    Revert "3Lesson Aded"
    This reverts commit 9bf780a3e29bdb2bdd8a1d485aa237e78396e5f5.

--- Lesson3/Dz3.2PonomarevP.py
+++
@@ -1,6 +0,0 @@
-def my_func(**kwargs):
-    print(f'Данные о персоне: {kwargs}')
-
-
-person = input('Введите Имя, Фамилию, Год рождения, Город проживания, e-mail и Телефон через пробел: ').split()
-my_func(name=person[0], Фамилия=person[1], Год_рождения=person[2], Город=person[3], e_mail=person[4], Телефон=person[5])
